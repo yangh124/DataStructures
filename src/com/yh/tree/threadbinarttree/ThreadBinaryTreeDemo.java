f877e5ab78e6ae0b187adb318468170ccc908892
yanghao <yangh@peoplecommnue.com>
2021-06-03 22:07:28 +0800

    遍历线索化二叉树

--- src/com/yh/tree/threadbinarttree/ThreadBinaryTreeDemo.java
+++ src/com/yh/tree/threadbinarttree/ThreadBinaryTreeDemo.java
@@ -9,7 +9,31 @@ package com.yh.tree.threadbinarttree;
 public class ThreadBinaryTreeDemo {
 
     public static void main(String[] args) {
-
+        HeroNode root = new HeroNode(1, "tom");
+        HeroNode node2 = new HeroNode(3, "jack");
+        HeroNode node3 = new HeroNode(6, "smith");
+        HeroNode node4 = new HeroNode(8, "mary");
+        HeroNode node5 = new HeroNode(10, "tim");
+        HeroNode node6 = new HeroNode(14, "lily");
+        root.setLeft(node2);
+        root.setRight(node3);
+        node2.setLeft(node4);
+        node2.setRight(node5);
+        node3.setLeft(node6);
+        //测试中序线索化二叉树
+        ThreadedBinaryTree threadedBinaryTree = new ThreadedBinaryTree();
+        threadedBinaryTree.setRoot(root);
+        threadedBinaryTree.threadedNodes();
+        //原先node5的左右子结点为null 中序线索化之后 左子结点变为node2 右子结点变为root
+        HeroNode left = node5.getLeft();
+        System.out.println(left);
+        HeroNode right = node5.getRight();
+        System.out.println(right);
+        /**
+         * 线索化二叉树之后使用原来的中序遍历会导致栈溢出
+         */
+        System.out.println("=============使用线索化方式遍历=============");
+        threadedBinaryTree.threadedList();
     }
 
 }
@@ -29,6 +53,10 @@ class ThreadedBinaryTree {
 
     public void setRoot(HeroNode root) {
         this.root = root;
+    }
+
+    public void threadedNodes() {
+        threadedNodes(root);
     }
 
     /**
@@ -56,10 +84,34 @@ class ThreadedBinaryTree {
             //修改前驱结点的右指针类型
             pre.setRightType(1);
         }
+        //每处理一个结点后，让当前结点是下一个结点的前驱结点
         pre = node;
         //3. 再线索化右子树
         threadedNodes(node.getRight());
     }
+
+    //遍历线索化二叉树方法
+    public void threadedList() {
+        //定义一个变量，存放当前遍历的结点，从root开始
+        HeroNode node = root;
+        while (node != null) {
+            //循环的找到leftType==1的结点，第一个找到的就是8结点
+            //后面随着遍历的变化，因为当leftType==1时，说明该结点是按照线索化处理后的有效结点
+            while (node.getLeftType() == 0) {
+                node = node.getLeft();
+            }
+            //打印当前结点
+            System.out.println(node);
+            //如果当前结点有后继结点，就输出
+            while (node.getRightType() == 1) {
+                //获取当前结点的后继结点
+                node = node.getRight();
+                System.out.println(node);
+            }
+            node = node.getRight();
+        }
+    }
+
 
     public void delNode(int no) {
         if (root == null) {
