a69a6919ac2027aec7929942db94af7883bc750d
yanghao <yangh@peoplecommnue.com>
2021-06-07 09:31:18 +0800

    堆排序

---
+++ src/com/yh/tree/HeapSort.java
@@ -0,0 +1,34 @@
+package com.yh.tree;
+
+/**
+ * @author : yh
+ * @date : 2021/6/6 12:24
+ */
+public class HeapSort {
+
+    public static void main(String[] args) {
+        //将数组升序排序
+        int[] arr = {4, 6, 8, 5, 9};
+
+    }
+
+    /**
+     * 堆排序
+     *
+     * @param arr 数组
+     */
+    public static void heapSort(int[] arr) {
+
+    }
+
+    /**
+     * 将树调整成大顶堆
+     *
+     * @param arr    待调整的数组
+     * @param i      表示非叶子结点在数组中的索引
+     * @param length 表示对多少个元素调整，length是在逐渐的减少
+     */
+    public static void adjustHeap(int[] arr, int i, int length) {
+
+    }
+}
